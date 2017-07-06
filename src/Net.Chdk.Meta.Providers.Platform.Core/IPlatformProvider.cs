6bd678e70ec196b5c34c3400e0af147c90abb1bf
Dmitry Shechtman <damnian@gmail.com>
2017-07-06 22:07:57 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.Platform.Core/IPlatformProvider.cs
@@ -0,0 +1,11 @@
+﻿using Net.Chdk.Meta.Model.CameraModel;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Meta.Providers.Platform
+{
+    public interface IPlatformProvider
+    {
+        IDictionary<string, PlatformData> GetPlatforms(Stream stream);
+    }
+}
