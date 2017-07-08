5cf67978f9295f713862e211400e8705918f5c0e
Dmitry Shechtman <damnian@gmail.com>
2017-07-08 06:54:46 +0300

    Replace stream with path

--- src/Net.Chdk.Meta.Providers.Platform.Core/IPlatformProvider.cs
+++ src/Net.Chdk.Meta.Providers.Platform.Core/IPlatformProvider.cs
@@ -1,11 +1,10 @@
 ﻿using Net.Chdk.Meta.Model.CameraModel;
 using System.Collections.Generic;
-using System.IO;
 
 namespace Net.Chdk.Meta.Providers.Platform
 {
     public interface IPlatformProvider
     {
-        IDictionary<string, PlatformData> GetPlatforms(Stream stream);
+        IDictionary<string, PlatformData> GetPlatforms(string path);
     }
 }
