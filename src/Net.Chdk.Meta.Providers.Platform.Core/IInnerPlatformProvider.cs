cfd1ad9392ab259a999d3f9c17b269e03b77fc55
Dmitry Shechtman <damnian@gmail.com>
2017-07-13 00:14:41 +0300

    Add IInnerPlatformProvider

---
+++ src/Net.Chdk.Meta.Providers.Platform.Core/IInnerPlatformProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Net.Chdk.Meta.Providers.Platform
+{
+    public interface IInnerPlatformProvider
+    {
+        IEnumerable<KeyValuePair<string, string>> DoGetPlatforms(TextReader reader);
+        string Extension { get; }
+    }
+}
